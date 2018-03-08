3212389a07089465171f5a195fdc16bac551ac54
Денис Шарапов <denisharapov@yandex.ru>
2018-03-08 12:45:28 +0700

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 Redmine::Plugin.register :alpha_cards do
-  name 'SEMAT Alpha Cards plugin'
+  name 'SEMAT Alpha State Cards plugin'
   author 'Kreosoft'
-  description 'Adds Ivar Jacobson\'s SEMAT Alpha cards'
+  description 'Adds Ivar Jacobson\'s SEMAT Alpha State Cards to Redmine'
   version '1.0.0'
-  url 'http://kreosoft.ru'
-  author_url 'http://kreosoft.ru'
+  url 'https://git.kreosoft.ru/public-tsu/Alphacards'
+  author_url 'https://git.kreosoft.ru/public-tsu/Alphacards'
 
   permission :alphacards, { :alphacards => [:index, :updatedata, :updateposition] }, :public => true
   menu :project_menu, :alphacards, { :controller => 'alphacards', :action => 'index' }, :caption => 'Alpha cards', :after => :activity, :param => :project_id
